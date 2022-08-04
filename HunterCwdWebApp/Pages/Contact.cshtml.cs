a30603804ec0245f72c25f02eaf8f0803e8c3067
Alec234 <30980378+Alec234@users.noreply.github.com>
2022-08-04 14:12:07 -0500

    Adding Contact page and footer link

---
+++ HunterCwdWebApp/Pages/Contact.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HunterCwdWebApp.Pages
+{
+    public class ContactModel : PageModel
+    {
+        [BindProperty]
+        public string fullName { get; set; }
+        [BindProperty]
+        public string email { get; set; }
+        [BindProperty]
+        public string subject { get; set; }
+        [BindProperty]
+        public string message { get; set; }
+        public void OnGet()
+        {
+           //do something
+        }
+    }
+}
