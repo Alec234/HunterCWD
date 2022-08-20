9cfbc45ace33666440caa5d437f634169d668de5
Alec_Symmonds <symmondsalec@gmail.com>
2022-08-20 18:46:58 -0500

    Adding in email functionality

--- HunterCwdWebApp/Pages/Contact.cshtml.cs
+++ HunterCwdWebApp/Pages/Contact.cshtml.cs
@@ -1,5 +1,8 @@
+using MailKit.Net.Smtp;
+using MailKit.Security;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MimeKit;
 
 namespace HunterCwdWebApp.Pages
 {
@@ -20,7 +23,40 @@ namespace HunterCwdWebApp.Pages
 
         public void OnPost()
         {
-            //do something
+            //call email service
+
+           /* SmtpClient smtpClient = new("smtp.gmail.com", 587);
+            //smtpClient.Host = "localhost";
+            //what to do with the fullName... store as a known user? add them to a mailing list?
+            //string fullName = this.fullName;
+            string sender = this.email;
+            string recipient = "symmondsalec@gmail.com";
+            string subject = this.subject;
+            string message = this.message;
+
+            return smtpClient.SendMailAsync(sender, recipient, subject, message);*/
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(this.fullName, this.email));
+            message.To.Add(new MailboxAddress("HunterCWD", "hutnersCWD@gmail.com"));
+            message.Subject = this.subject;
+
+            message.Body = new TextPart("plain")
+            {
+                Text = this.message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate("hutnersCWD@gmail.com", "Fuck0ff!");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
+
         }
     }
 }
